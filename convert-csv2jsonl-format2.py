c1236767c5449433ed97ab1cfa6406ce5d32682f
Akiva Miura <akiva.miura@gmail.com>
2024-02-13 14:32:52 +0900

    fix(convert-csv2jsonl-format2.py): fix variable name from q_id to ID to improve clarity and consistency feat(convert-csv2jsonl-format2.py): add support for custom ID prefix and index length to generate IDs with a configurable format

--- convert-csv2jsonl-format2.py
+++ convert-csv2jsonl-format2.py
@@ -18,6 +18,8 @@ EXCLUDE_COLUMNS = [
 def convert_tsv2jsonl(
     input_file,
     jsonl_file,
+    id_prefix,
+    index_length,
     encoding = 'utf-8',
     tsv = False,
 ):
@@ -28,7 +30,8 @@ def convert_tsv2jsonl(
         delimiter = ','
     with open(input_file, 'r', encoding=encoding) as tsvfile:
         reader = csv.DictReader(tsvfile, delimiter=delimiter)
-        with open(jsonl_file, 'w') as jsonlfile:
+        rows = []
+        with open(jsonl_file, 'w', encoding='utf-8') as jsonlfile:
             for i, row in enumerate(
                 tqdm(reader),
             ):
@@ -37,9 +40,8 @@ def convert_tsv2jsonl(
                 #    logger.debug('row.keys(): %s', row.keys())
                 #if i >= 1:
                 #    break
-                #q_id = row['QuestionID']
+                q_id = row['QuestionID']
                 #a_id = row['AnswerID']
-                q_id = int(row['QuestionID'])
                 q = row['質問']
                 a = row['回答']
                 risk_area = row['risk-area']
@@ -51,9 +53,9 @@ def convert_tsv2jsonl(
                     if exclude_column in row:
                         del row[exclude_column]
                 #id = 'answercarefully-instruction-000-001-0000001-001'
-                id = f'answercarefully-instruction-000-001-0000001-{q_id:03d}'
+                #id = f'answercarefully-instruction-000-001-0000001-{q_id:03d}'
                 d = dict(
-                    ID=id,
+                    ID=q_id,
                     text=q,
                     output=a,
                     meta = {
@@ -63,7 +65,19 @@ def convert_tsv2jsonl(
                     },
                 )
                 #logger.debug('d: %s', d)
-                jsonlfile.write(json.dumps(d, ensure_ascii=False) + '\n')
+                #jsonlfile.write(json.dumps(d, ensure_ascii=False) + '\n')
+                rows.append(d)
+            max_index_length = len(str(len(rows)))
+            if index_length < max_index_length:
+                logger.error('index_length must be greater than or equal to %d', max_index_length)
+                return
+            for i, row in enumerate(
+                tqdm(rows),
+            ):
+                str_index = str(int(row['ID'])).zfill(index_length)
+                id = f'{id_prefix}-{str_index}-001'
+                row['ID'] = id
+                jsonlfile.write(json.dumps(row, ensure_ascii=False) + '\n')
 
 if __name__ == '__main__':
     import argparse
@@ -72,10 +86,14 @@ if __name__ == '__main__':
     parser.add_argument('jsonl_file', help='Output JSONL file')
     parser.add_argument('--encoding', '-E', default='utf-8', help='Encoding of the TSV file')
     parser.add_argument('--tsv', '-T', action='store_true', help='Input TSV file')
+    parser.add_argument('--id-prefix', '-P', required=True, help='ID prefix')
+    parser.add_argument('--index-length', '-L', type=int, required=True, help='Index Length')
     args = parser.parse_args()
     convert_tsv2jsonl(
         args.input_file,
         args.jsonl_file,
         encoding=args.encoding,
         tsv=args.tsv,
+        id_prefix=args.id_prefix,
+        index_length=args.index_length,
     )
