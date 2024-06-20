ac5f7b33d58a29a946bf9356a4cb5131c4f5f8fe
Akiva Miura <akiva.miura@gmail.com>
2024-06-20 20:02:37 +0900

    修正(convert-json2table.py): 'meta'フィールドの取得方法を変更 修正(convert-json2table.py): 'set_value'関数にNoneチェックを追加 新機能(convert-json2table.py): 'question-ID'と'point-allocation'フィールドのサポートを追加

--- convert-json2table.py
+++ convert-json2table.py
@@ -6,6 +6,8 @@ import pandas as pd
 from logzero import logger
 
 def set_value(d, j, key, delimiter=None):
+    if j is None:
+        return
     if key not in j:
         return
     value = j[key]
@@ -39,7 +41,8 @@ def export_to_table(
         else:
             raise ValueError(f'Invalid input_json_path: {input_json_path}')
         for row in rows:
-            meta = row['meta']
+            #meta = row['meta']
+            meta = row.get('meta')
             flat_row = {}
             #flat_row = {
             #    'ID': row['ID'],
@@ -57,11 +60,12 @@ def export_to_table(
             #    #'output-reference': str.join('\n', meta['output-reference']),
             #    'output-reference': output_reference,
             #}
-            set_value(flat_row, row, 'ID')
+            set_value(flat_row, row,  'ID')
+            set_value(flat_row, row,  'question-ID')
             set_value(flat_row, meta, 'text-producer')
             set_value(flat_row, meta, 'output-producer')
-            set_value(flat_row, row, 'text')
-            set_value(flat_row, row, 'output')
+            set_value(flat_row, row,  'text')
+            set_value(flat_row, row,  'output')
             set_value(flat_row, meta, 'task', delimiter=';')
             set_value(flat_row, meta, 'perspective', delimiter=';')
             set_value(flat_row, meta, 'time-dependency')
@@ -70,6 +74,7 @@ def export_to_table(
             set_value(flat_row, meta, 'output-type', delimiter=';')
             set_value(flat_row, meta, 'alert-type', delimiter=';')
             set_value(flat_row, meta, 'output-reference', delimiter='\n')
+            set_value(flat_row, meta, 'point-allocation', delimiter=';')
             all_rows.append(flat_row)
     df = pd.DataFrame(all_rows)
     logger.debug('df: %s', df)
